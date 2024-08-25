2294dc8aa5fa8828c966d2ccbcc44d354ad02a5b
Ahmed Amin <151462504+Ahmed2001681@users.noreply.github.com>
2024-08-25 11:05:09 +0500

    Update calculator.js
    second commit

--- calculator.js
+++ calculator.js
@@ -1,13 +1,27 @@
-function deleteLastCharacter() {
-      var display = document.getElementById('display');
-      display.value = display.value.slice(0, -1);
-    }
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-    function calculate() {
-      var display = document.getElementById('display');
-      try {
-        display.value = eval(display.value);
-      } catch (err) {
-        display.value = 'Error';
-      }
-    }
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
